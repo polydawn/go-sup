9541bb92c446227f074bd323178d07264248d4bc
Eric Myhre <hash@exultant.us>
2016-04-25 03:07:43 -0500

    Thinking-out-loud comments on error handling.
    We're getting towards the tough meaty bits.  We started with some promise-pattern style stuff, but part of what I want from this system is (remember) better *worst* case behavior... including for situations like when you forgot to check the promise for returning an error.  So can we make a default behavior to up-sub problems unless they were handled antefiling?  I dunno.  Maybe.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- supervisor_internal.go
+++ supervisor_internal.go
@@ -50,6 +50,16 @@ func (svr *Supervisor) run(superFn SupervisonFn) {
 		err := recover()
 		if err != nil {
 			// TODO uffdah
+			// So this needs to implode aggressively -- pushing the maint actor into a new (higher priority) state, which then starts firing cancels down.
+			// It also needs somewhere to boil out itself.  And we kinda left that behind, somehow.  :(
+
+			// ... Is there something we should do to boil out errors for children that are clearly not being witnessed by the controller that spawned them?
+			// ..... Probably?
+			// ........ Does that mean we should actually make that *normal*, and you have to intercept explicitly if you want to handle it better?
+			//    You can't really do that very well.  It's hard to force the controller to panic.  (This is the interruption-impossibility limit, in fact.)
+			//     But we could still certainly push errors *up* by default, so the next level higher supervisory code can react promptly (and then
+			//      if your process that didn't handle it wants to be the badly behaved non-cancel-compliant one when that parent shuts down, that's
+			//      fine/inevitable, and it'll be correctly reported as such (well, if we can build watchdogs that good, anyway, which is still in the "hope" phase).
 		}
 		// it's over.  wind'r all down.
 		svr.ctrlChan_winddown <- struct{}{}

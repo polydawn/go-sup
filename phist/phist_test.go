4e3e8911b7d715d3f6ef11c2ded4d52004e981f1
Eric Myhre <hash@exultant.us>
2016-05-14 18:51:29 -0500

    I need some Things to help test situations where events *correctly* have multiple valid orderings because the operations are intentionally nonlinearizable.
    This is aiming for "simplest thing that works".  A "better" solution probably approaches the same complexity as a regexp engine or a lexer state machine, which... while being more correct for a more complex category of inputs, would be proportionally less informative when it failed.  So, this: a series of flat sequence checks, going for easy to write and clear to debug.
    
    Note that this also doesn't help in the slighest at *confirming* that multiple routes may be taken.  That would be... possible, but not the greatest use of CPU time.  Instead, it's better to plan other tests which force concurrent operations and would either immediately fail (or at least clearly deadlock) so that we can complete tests without burning silicon.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

---
+++ phist/phist_test.go
@@ -0,0 +1,62 @@
+package phist
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func Test(t *testing.T) {
+	Convey("ShouldSequence should check sequencing", t, func() {
+		Convey("A slice which contains the elements in correct sequence should pass", func() {
+			So(
+				ShouldSequence(
+					[]string{"a", "c", "b"},
+					"a", "b",
+				),
+				ShouldEqual,
+				"",
+			)
+		})
+		Convey("Elements in an incorrect order fail", func() {
+			So(
+				ShouldSequence(
+					[]string{"b", "a", "c"},
+					"a", "b",
+				),
+				ShouldEqual,
+				`Sequence broken: at index 0: "b" occured 1 times, overtaking "a" which is supposed to precede it but only occured 0`,
+			)
+		})
+		Convey("Recurring elements pass as long as they can be pairwise ordered", func() {
+			So(
+				ShouldSequence(
+					[]string{"a", "c", "a", "b", "b"},
+					"a", "b",
+				),
+				ShouldEqual,
+				"",
+			)
+		})
+		Convey("Recurring elements fail if later parts of the sequence outnumber their precursors", func() {
+			So(
+				ShouldSequence(
+					[]string{"a", "c", "b", "b", "a"},
+					"a", "b",
+				),
+				ShouldEqual,
+				`Sequence broken: at index 3: "b" occured 2 times, overtaking "a" which is supposed to precede it but only occured 1`,
+			)
+		})
+		Convey("Recurring elements fail if sequences start but lack matches", func() {
+			So(
+				ShouldSequence(
+					[]string{"a", "c", "a", "b", "e"},
+					"a", "b",
+				),
+				ShouldEqual,
+				`Sequence broken: at end, "a" occured 2 times, and "b" which is supposed to follow it only occured 1`,
+			)
+		})
+	})
+}

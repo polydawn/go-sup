ab4c003ad2db4cd86ac44328b0e5fee38e18e410
Eric Myhre <hash@exultant.us>
2016-07-22 12:27:39 -0500

    Port saltmines example to new api.
    Unintending a massive block of code that ended up in an anonymous func feels good.  Not having the witness objects to hurl around feels ok but only because I wasn't using them for anything useful (I think that's going to remain defensible though).
    
    The test is overall in need of More -- there's not enough of it done to really verify much about the series of steps we're hitting; only to roughly confirm whether we successfully avoid dead-(or live-)lock -- but at least it's as functional as it was before.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- example/saltmines/main.go
+++ example/saltmines/main.go
@@ -9,55 +9,57 @@ import (
 )
 
 func Main(stdin io.Reader, stderr io.Writer) {
-	sup.NewRootSupervisor(func(svr sup.Supervisor) {
-		// I'm a very lazy controller, and I mostly delegate work to others.
-		// I don't actually even wake up if something goes wrong!
-		// I assume things are going according to plan unless something
-		//  really terrible gets so far that it sets my office on fire around me.
-		// Even then: honestly, I'm already in the Cayman Islands.  There's
-		//  nobody in the head office anymore.  Really, all the other workers
-		//  need is the idea that there's someone they *could* complain to.
-		// If somebody actually *does* file a report, the mail will make
-		//  it to the corporate franchise office, somehow.  (Maybe the dutiful
-		//  secretary I left behind will actually do the maint work for me,
-		//  even though I've nipped off.)
-		fmt.Fprintf(stderr, "Owner: hello\n")
+	// I'm a very lazy controller, and I mostly delegate work to others.
+	// I don't actually even wake up if something goes wrong!
+	// I assume things are going according to plan unless something
+	//  really terrible gets so far that it sets my office on fire around me.
+	// Even then: honestly, I'm already in the Cayman Islands.  There's
+	//  nobody in the head office anymore.  Really, all the other workers
+	//  need is the idea that there's someone they *could* complain to.
+	// If somebody actually *does* file a report, the mail will make
+	//  it to the corporate franchise office, somehow.  (Maybe the dutiful
+	//  secretary I left behind will actually do the maint work for me,
+	//  even though I've nipped off.)
+	fmt.Fprintf(stderr, "Owner: hello\n")
 
-		// There are four major operations going on under my domain:
-		//   - The mining pits -- these produce a steady stream of "slag"
-		//   - The ore washing plants -- these do some basic processing, and route out several different kinds of "ore"
-		//   - The foundries -- there's several different kinds of these, they take only specific kinds of "ore"
-		//   - The shipping wharf -- this station packages up all the ingots into crates for sale
-		// Keep an eye on the ore washing plants.  Sometimes they get jammed,
-		//  and we have to take one out of service, scrap it for parts, and
-		//  just install a whole new one without any of the wear-n-tear.
-		// There's also a fivth operation: the oversight office.
-		//  The oversight office can sometimes get letters from other parts
-		//  of Her Majesty's Goverment, to which the office is required to
-		//  respond in a timely fashion.  Sometimes this requires the
-		//  oversight office to commission a team to gather a report.  Such
-		//  teams tend to be short-lived, but they may ask questions about
-		//  (or sometimes give odd orders to) the other three major operational
-		//  centers of our production pipeline.
-		slagPipe := make(chan Slag)
-		minePit := &MinePits{
-			thePit:   stdin,
-			slagPipe: slagPipe,
-		}
-		minePitWitness := svr.NewSupervisor(minePit.Run)
-		minePitWitness.Cancel()
+	// There are four major operations going on under my domain:
+	//   - The mining pits -- these produce a steady stream of "slag"
+	//   - The ore washing plants -- these do some basic processing, and route out several different kinds of "ore"
+	//   - The foundries -- there's several different kinds of these, they take only specific kinds of "ore"
+	//   - The shipping wharf -- this station packages up all the ingots into crates for sale
+	// Keep an eye on the ore washing plants.  Sometimes they get jammed,
+	//  and we have to take one out of service, scrap it for parts, and
+	//  just install a whole new one without any of the wear-n-tear.
+	// There's also a fivth operation: the oversight office.
+	//  The oversight office can sometimes get letters from other parts
+	//  of Her Majesty's Goverment, to which the office is required to
+	//  respond in a timely fashion.  Sometimes this requires the
+	//  oversight office to commission a team to gather a report.  Such
+	//  teams tend to be short-lived, but they may ask questions about
+	//  (or sometimes give odd orders to) the other three major operational
+	//  centers of our production pipeline.
+	rootSvr, triggerWrapup := sup.NewSupervisor()
+	mgr := sup.NewManager(rootSvr)
 
-		oreWasher := &OreWashingFacility{
-			slagPipe:     slagPipe,
-			copperHopper: make(chan OreCopper),
-			tinHopper:    make(chan OreTin),
-			zincHopper:   make(chan OreZinc),
-		}
-		oreWasherWitness := svr.NewSupervisor(oreWasher.Run)
-		oreWasherWitness.Cancel()
+	slagPipe := make(chan Slag)
+	minePit := &MinePits{
+		thePit:   stdin,
+		slagPipe: slagPipe,
+	}
+	go mgr.NewTask().Run(minePit.Run)
+	//minePitWitness.Cancel()
 
-		fmt.Fprintf(stderr, "Owner: leaving for cayman\n")
-	})
+	oreWasher := &OreWashingFacility{
+		slagPipe:     slagPipe,
+		copperHopper: make(chan OreCopper),
+		tinHopper:    make(chan OreTin),
+		zincHopper:   make(chan OreZinc),
+	}
+	go mgr.NewTask().Run(oreWasher.Run)
+	//oreWasherWitness.Cancel()
+
+	triggerWrapup()
+	mgr.Work()
 }
 
 type (
@@ -91,9 +93,9 @@ func (mp *MinePits) Run(svr sup.Supervisor) {
 			// careful.  you have to put nb/cancellable selects for each send, too.
 			select {
 			case mp.slagPipe <- Slag(scanner.Text()):
-			case <-svr.SelectableQuit():
+			case <-svr.QuitCh():
 			}
-		case <-svr.SelectableQuit():
+		case <-svr.QuitCh():
 			return
 		}
 	}
@@ -111,11 +113,11 @@ func (owf *OreWashingFacility) Run(svr sup.Supervisor) {
 	//  some time; this can strike fairly randomly, so we run a bunch
 	//  of processing separately to even things out.
 	// That means *we're* a supervisor for all those parallel processors.
-	var children []sup.Witness
+	mgr := sup.NewManager(svr)
 	for n := 0; n < 4; n++ {
-		wit := svr.NewSupervisor(owf.runSingleStation)
-		children = append(children, wit)
+		go mgr.NewTask().Run(owf.runSingleStation)
 	}
+	mgr.Work()
 }
 
 func (owf *OreWashingFacility) runSingleStation(svr sup.Supervisor) {
@@ -129,22 +131,22 @@ func (owf *OreWashingFacility) runSingleStation(svr sup.Supervisor) {
 			case "copper":
 				select {
 				case owf.copperHopper <- OreCopper(slag):
-				case <-svr.SelectableQuit():
+				case <-svr.QuitCh():
 				}
 			case "tin":
 				select {
 				case owf.tinHopper <- OreTin(slag):
-				case <-svr.SelectableQuit():
+				case <-svr.QuitCh():
 				}
 			case "zinc":
 				select {
 				case owf.zincHopper <- OreZinc(slag):
-				case <-svr.SelectableQuit():
+				case <-svr.QuitCh():
 				}
 			default:
-				panic("unknown ore type, cannot sort")
+				panic(fmt.Sprintf("unknown ore type %q, cannot sort", slag))
 			}
-		case <-svr.SelectableQuit():
+		case <-svr.QuitCh():
 			return
 		}
 	}

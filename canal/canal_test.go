c549f216772ba53aed7f5b80075f09e677b8efd1
Eric Myhre <hash@exultant.us>
2016-07-26 04:04:56 -0500

    Implement a 'canal' type for bilaterally nonblocking handoffs.
    At the cost of unbounded memory costs, sure.  There really are times when I'm OK with that, though.  For example, I'm thinking this might be useful for buffering the error/exit reports from child procs: definitely don't wanna drop em; badly need the submitter to not ever block; and needs selectability on the reader side.  The next closest fit would be a channel with a "this is high enough" capacity.  This is far less kludgy.
    
    "Canal" because it's a like a channel, but doesn't really care if the waters get high and aren't flowing particularly fast.  Name subject to change though.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

---
+++ canal/canal_test.go
@@ -0,0 +1,52 @@
+package canal
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func Test(t *testing.T) {
+	Convey("Canal can...", t, func() {
+		gondola := New()
+
+		Convey("pump values", func() {
+			gondola.Push("x")
+			gondola.Push("y")
+			gondola.Push("z")
+			So(<-gondola.Next(), ShouldEqual, "x")
+			So(<-gondola.Next(), ShouldEqual, "y")
+			So(<-gondola.Next(), ShouldEqual, "z")
+		})
+
+		Convey("block when empty", func() {
+			var answered bool
+			select {
+			case <-gondola.Next():
+				answered = true
+			default:
+				answered = false
+			}
+			So(answered, ShouldEqual, false)
+
+			Convey("answers even dropped channels", func() {
+				secondReq := gondola.Next()
+				gondola.Push("1")
+				// we still don't expect an answer,
+				//  because the "1" routed to the channel in the prev test.
+				select {
+				case <-secondReq:
+					answered = true
+				default:
+					answered = false
+				}
+				So(answered, ShouldEqual, false)
+
+				Convey("definitely answers eventually", func() {
+					gondola.Push("2")
+					So(<-secondReq, ShouldEqual, "2")
+				})
+			})
+		})
+	})
+}

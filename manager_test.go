ecaa7b603d45c2cdac98cd1bdac7161c4b1ef4c8
Eric Myhre <hash@exultant.us>
2016-07-30 21:07:14 -0500

    Tests in parallel actions.
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- manager_test.go
+++ manager_test.go
@@ -1,6 +1,7 @@
 package sup
 
 import (
+	"sort"
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
@@ -11,6 +12,7 @@ func TestManager(t *testing.T) {
 		rootWrit := NewTask()
 		rootWrit.Run(func(super Supervisor) {
 			mgr := NewManager(super)
+
 			Convey("And some serially executing tasks", func() {
 				ch := make(chan string, 2)
 				mgr.NewTask("1").Run(ChanWriterAgent("1", ch))
@@ -25,6 +27,27 @@ func TestManager(t *testing.T) {
 					mgr.Work()
 					// maybe not the most useful test
 					So(mgr.(*manager).doneFuse.IsBlown(), ShouldBeTrue)
+					So(mgr.(*manager).wards, ShouldHaveLength, 0)
+				})
+			})
+
+			Convey("And some parallel executing tasks", func() {
+				ch := make(chan string, 2)
+				go mgr.NewTask("1").Run(ChanWriterAgent("1", ch))
+				go mgr.NewTask("2").Run(ChanWriterAgent("2", ch))
+
+				Convey("Tasks run and we see their sideeffects", func() {
+					results := []string{<-ch, <-ch}
+					sort.Strings(results)
+					So(results[0], ShouldEqual, "1")
+					So(results[1], ShouldEqual, "2")
+				})
+
+				Convey("Manager.Work should gather all the things", func() {
+					mgr.Work()
+					// maybe not the most useful test
+					So(mgr.(*manager).doneFuse.IsBlown(), ShouldBeTrue)
+					So(mgr.(*manager).wards, ShouldHaveLength, 0)
 				})
 			})
 		})
